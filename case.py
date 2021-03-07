50d640306d2bf7007e50da2c5500f18fd0e454d5
evanazirova12 <eva20th@mail.ru>
2021-03-07 14:32:50 +0700

    Formatted output to file 'output.txt'.

--- case.py
+++ case.py
@@ -13,24 +13,24 @@ with open('input.txt', 'r') as f_in:
             s = f.read()
             text = str(s)
             part_name = text.find("nfl-c-player-header__title")
-            name = text[text.find('>', part_name) + 1:text.find('</h1', part_name)]
+            name = int(text[text.find('>', part_name) + 1:text.find('</h1', part_name)])
 
             part_COMP = text.find('passingCompletions')
-            COMP = text[text.find('>', part_COMP) + 25:text.find('<', part_COMP) - 22]
+            COMP = int(text[text.find('>', part_COMP) + 25:text.find('<', part_COMP) - 22])
 
             part_ATT = text.find('passingAttempts')
-            ATT = text[text.find('>', part_ATT) + 25:text.find('<', part_ATT) - 22]
+            ATT = int(text[text.find('>', part_ATT) + 25:text.find('<', part_ATT) - 22])
 
             part_YDS = text.find('passingYards')
-            YDS = text[text.find('>', part_YDS) + 25:text.find('<', part_YDS) - 22]
+            YDS = int(text[text.find('>', part_YDS) + 25:text.find('<', part_YDS) - 22])
 
             part_TD = text.find('passingTouchdowns')
-            TD = text[text.find('>', part_TD) + 25:text.find('<', part_TD) - 22]
+            TD = int(text[text.find('>', part_TD) + 25:text.find('<', part_TD) - 22])
 
             part_INT = text.find('passingInterceptions')
-            INT = text[text.find('>', part_INT) + 25:text.find('<', part_INT) - 22]
+            INT = int(text[text.find('>', part_INT) + 25:text.find('<', part_INT) - 22])
 
             part_PR = text.find('passingPasserRating')
-            PR = text[text.find('>', part_PR) + 25:text.find('<', part_PR) - 22]
+            PR = float(text[text.find('>', part_PR) + 25:text.find('<', part_PR) - 22])
 
             out_f.writelines('{0:<20} {1:<7} {2:<7} {3:<7} {4:<7} {5:<7} {6:<7.2f}\n'.format(name, COMP, ATT, YDS, TD, INT, PR))
